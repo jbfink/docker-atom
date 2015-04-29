b8389427ec55250e1adb83cfb31ab1c2c830bc38
jbfink <john.fink@gmail.com>
2015-04-29 16:02:50 -0400

    WORKING

--- mysql/docker-entrypoint.sh
+++ mysql/docker-entrypoint.sh
@@ -35,6 +35,9 @@ if [ "$1" = 'mysqld' ]; then
 			CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
 			DROP DATABASE IF EXISTS test ;
+			DROP DATABASE IF EXISTS atom ;
+			CREATE DATABASE atom CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+			GRANT INDEX, CREATE, SELECT, INSERT, UPDATE, DELETE, ALTER, LOCK TABLES ON atom.* TO 'atom'@'%' IDENTIFIED BY 'worstpass';
 		EOSQL
 		
 		if [ "$MYSQL_DATABASE" ]; then
